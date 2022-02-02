fcfce8340c9e52d0430de45856ba084cd6e5280f
Joe McPartland <joemcpartland@gmail.com>
2022-02-02 09:09:50 -0700

    commit tree 2d309a4ae0940a63459bbdd9ecab24f2e181b27f

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,16 @@
 import React from "react";
 
-function Search() {
+function Search({ onSearchChange, searchTerm }) {
+
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
       <input
         type="text"
         id="search"
+        value={searchTerm}
         placeholder="Type a name to search..."
-        onChange={(e) => console.log("Searching...")}
+        onChange={(e) => onSearchChange(e.target.value)}
       />
     </div>
   );
