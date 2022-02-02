d806e5151410658c8fbd8be33f3a366423dcef2a
Joe McPartland <joemcpartland@gmail.com>
2022-02-02 09:11:01 -0700

    Finshed deliverables

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewPlantForm() {
+function NewPlantForm({ onAddPlant }) {
+  const [formData, setFormData] = useState({
+    name: "",
+    image: "",
+    price: "",
+  })
+
+  const  handleChange = (e) => {
+    setFormData({...formData, [e.target.name]:e.target.value})
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault(e)
+    onAddPlant(formData)
+  }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          name="name" 
+          value={formData.name}
+          onChange={handleChange}
+          placeholder="Plant name" />
+        <input 
+          type="text" 
+          name="image" 
+          value={formData.image}
+          onChange={handleChange}
+          placeholder="Image URL" 
+        />
+        <input 
+          type="number" 
+          name="price" 
+          value={formData.price}
+          onChange={handleChange}
+          step="0.01" 
+          placeholder="Price" 
+        />
         <button type="submit">Add Plant</button>
       </form>
     </div>
